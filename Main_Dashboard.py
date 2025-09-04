9ae259433411a7e7d285ebdfa7d517d4fae83b38
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-04 21:42:31 +0330

    Update Main_Dashboard.py

--- Main_Dashboard.py
+++ Main_Dashboard.py
@@ -647,7 +647,7 @@ with col2:
     )
     st.plotly_chart(fig2, use_container_width=True)
 
-# --- Row 6 --------------------------------------------------------------------------------------------------------------
+# --- Row 6,left --------------------------------------------------------------------------------------------------------------
 @st.cache_data
 def load_bridgors_data(timeframe, start_date, end_date):
     start_str = start_date.strftime("%Y-%m-%d")
@@ -734,9 +734,138 @@ def load_bridgors_data(timeframe, start_date, end_date):
     """
     return pd.read_sql(query, conn)
 
-df_brg = load_bridgors_data(timeframe, start_date, end_date)
+# --- Row 6,right ---------------------------------------------------------------------------------------------------------
+@st.cache_data
+def load_bridgors_data_volume(timeframe, start_date, end_date):
+    start_str = start_date.strftime("%Y-%m-%d")
+    end_str = end_date.strftime("%Y-%m-%d")
 
-# --- Row (4): Charts ------------------------------------------------------------------------------------------------------
+    query = f"""
+    with squid_bridge as (
+    WITH axelar_service AS (
+  
+  SELECT 
+    created_at, 
+    LOWER(data:send:original_source_chain) AS source_chain, 
+    LOWER(data:send:original_destination_chain) AS destination_chain,
+    recipient_address AS user, 
+
+    CASE 
+      WHEN IS_ARRAY(data:send:amount) THEN NULL
+      WHEN IS_OBJECT(data:send:amount) THEN NULL
+      WHEN TRY_TO_DOUBLE(data:send:amount::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:send:amount::STRING)
+      ELSE NULL
+    END AS amount,
+
+    CASE 
+      WHEN IS_ARRAY(data:send:amount) OR IS_ARRAY(data:link:price) THEN NULL
+      WHEN IS_OBJECT(data:send:amount) OR IS_OBJECT(data:link:price) THEN NULL
+      WHEN TRY_TO_DOUBLE(data:send:amount::STRING) IS NOT NULL AND TRY_TO_DOUBLE(data:link:price::STRING) IS NOT NULL 
+        THEN TRY_TO_DOUBLE(data:send:amount::STRING) * TRY_TO_DOUBLE(data:link:price::STRING)
+      ELSE NULL
+    END AS amount_usd,
+
+    CASE 
+      WHEN IS_ARRAY(data:send:fee_value) THEN NULL
+      WHEN IS_OBJECT(data:send:fee_value) THEN NULL
+      WHEN TRY_TO_DOUBLE(data:send:fee_value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:send:fee_value::STRING)
+      ELSE NULL
+    END AS fee,
+
+    id, 
+    'Token Transfers' AS "Service", 
+    data:link:asset::STRING AS raw_asset
+
+  FROM axelar.axelscan.fact_transfers
+  WHERE status = 'executed'
+    AND simplified_status = 'received'
+    AND (
+    sender_address ilike '%0xce16F69375520ab01377ce7B88f5BA8C48F8D666%' -- Squid
+    or sender_address ilike '%0x492751eC3c57141deb205eC2da8bFcb410738630%' -- Squid-blast
+    or sender_address ilike '%0xDC3D8e1Abe590BCa428a8a2FC4CfDbD1AcF57Bd9%' -- Squid-fraxtal
+    or sender_address ilike '%0xdf4fFDa22270c12d0b5b3788F1669D709476111E%' -- Squid coral
+    or sender_address ilike '%0xe6B3949F9bBF168f4E3EFc82bc8FD849868CC6d8%' -- Squid coral hub
+    ) 
+
+  UNION ALL
+
+  SELECT  
+    created_at,
+    LOWER(data:call.chain::STRING) AS source_chain,
+    LOWER(data:call.returnValues.destinationChain::STRING) AS destination_chain,
+    data:call.transaction.from::STRING AS user,
+
+    CASE 
+      WHEN IS_ARRAY(data:amount) OR IS_OBJECT(data:amount) THEN NULL
+      WHEN TRY_TO_DOUBLE(data:amount::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:amount::STRING)
+      ELSE NULL
+    END AS amount,
+
+    CASE 
+      WHEN IS_ARRAY(data:value) OR IS_OBJECT(data:value) THEN NULL
+      WHEN TRY_TO_DOUBLE(data:value::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:value::STRING)
+      ELSE NULL
+    END AS amount_usd,
+
+    COALESCE(
+      CASE 
+        WHEN IS_ARRAY(data:gas:gas_used_amount) OR IS_OBJECT(data:gas:gas_used_amount) 
+          OR IS_ARRAY(data:gas_price_rate:source_token.token_price.usd) OR IS_OBJECT(data:gas_price_rate:source_token.token_price.usd) 
+        THEN NULL
+        WHEN TRY_TO_DOUBLE(data:gas:gas_used_amount::STRING) IS NOT NULL 
+          AND TRY_TO_DOUBLE(data:gas_price_rate:source_token.token_price.usd::STRING) IS NOT NULL 
+        THEN TRY_TO_DOUBLE(data:gas:gas_used_amount::STRING) * TRY_TO_DOUBLE(data:gas_price_rate:source_token.token_price.usd::STRING)
+        ELSE NULL
+      END,
+      CASE 
+        WHEN IS_ARRAY(data:fees:express_fee_usd) OR IS_OBJECT(data:fees:express_fee_usd) THEN NULL
+        WHEN TRY_TO_DOUBLE(data:fees:express_fee_usd::STRING) IS NOT NULL THEN TRY_TO_DOUBLE(data:fees:express_fee_usd::STRING)
+        ELSE NULL
+      END
+    ) AS fee,
+
+    id, 
+    'GMP' AS "Service", 
+    data:symbol::STRING AS raw_asset
+
+  FROM axelar.axelscan.fact_gmp 
+  WHERE status = 'executed'
+    AND simplified_status = 'received'
+    AND (
+        data:approved:returnValues:contractAddress ilike '%0xce16F69375520ab01377ce7B88f5BA8C48F8D666%' -- Squid
+        or data:approved:returnValues:contractAddress ilike '%0x492751eC3c57141deb205eC2da8bFcb410738630%' -- Squid-blast
+        or data:approved:returnValues:contractAddress ilike '%0xDC3D8e1Abe590BCa428a8a2FC4CfDbD1AcF57Bd9%' -- Squid-fraxtal
+        or data:approved:returnValues:contractAddress ilike '%0xdf4fFDa22270c12d0b5b3788F1669D709476111E%' -- Squid coral
+        or data:approved:returnValues:contractAddress ilike '%0xe6B3949F9bBF168f4E3EFc82bc8FD849868CC6d8%' -- Squid coral hub
+        ) 
+)
+
+SELECT created_at, id, user, amount_usd
+FROM axelar_service),
+
+first_tx as (
+select user, min(created_at::date) as first_timestamp
+from squid_bridge
+group by 1)
+
+
+select 
+  date_trunc('{timeframe}', created_at) as "Date",
+  case when a.user = b.user then 'New Users'
+  else 'Returning Users' end as "User Status",
+  round(sum(amount_usd)) as "Bridge Amount"
+from squid_bridge a left join first_tx b on a.created_at = b.first_timestamp
+where created_at::date>='{start_str}' and created_at::date<='{end_str}'
+group by 1,2
+order by 1
+    """
+    return pd.read_sql(query, conn)
+
+# --- Load Data -----------------------------------------------------------------------------------------------------------
+df_brg = load_bridgors_data(timeframe, start_date, end_date)
+df_brg_vol = load_bridgors_data_volume(timeframe, start_date, end_date)
+
+# --- Row (6): Charts ------------------------------------------------------------------------------------------------------
 col1, col2 = st.columns(2)
 
 with col1:
@@ -758,13 +887,13 @@ with col1:
     st.plotly_chart(fig_b1, use_container_width=True)
 
 with col2:
-    fig_b2 = go.Figure()
-    fig_b2.add_trace(go.Bar(
-        x=df_brg["Date"], y=df_brg["Bridgors Growth"], name="Bridgors Growth"
-    ))
-    fig_b2.update_layout(
-        title="Total New Bridgors Over Time",
-        yaxis=dict(title="Address count")
+    fig_stacked = px.bar(
+    df_brg_vol,
+    x="Date",
+    y="Bridge Amount",
+    color="User Status",
+    title="Bridge Volume by User Type Over Time"
     )
-    st.plotly_chart(fig_b2, use_container_width=True)
+    fig_stacked.update_layout(barmode="stack", yaxis_title="$USD")
+    st.plotly_chart(fig_stacked, use_container_width=True)
 
